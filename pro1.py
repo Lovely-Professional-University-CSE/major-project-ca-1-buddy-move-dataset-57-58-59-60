4e0b272967d6ea47a4323588bf47ba8b418c6af6
sssudarshan <56013986+sssudarshan@users.noreply.github.com>
2019-11-06 10:10:28 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,75 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Iris Data Loaded\n",
-      "Sample pattern: ['5.6' '3' '4.1' '1.3']\n",
-      "Class of the above pattern: Versicolor\n"
-     ]
-    }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "import math\n",
-    "import random\n",
-    "\n",
-    "pattern = []\n",
-    "classes = []\n",
-    "\n",
-    "filename = 'iris99.csv'\n",
-    "file = open(filename,'r')\n",
-    "#fopen=('iris99.csv','r')\n",
-    "\n",
-    "for line in file.readlines():\n",
-    "    row = line.strip().split(',')\n",
-    "    pattern.append(row[0:4])\n",
-    "    classes.append(row[4])\n",
-    "print(\"Iris Data Loaded\")\n",
-    "file.close\n",
-    "\n",
-    "pattern = np.array(pattern)\n",
-    "\n",
-    "sample_no = np.random.randint(0,len(pattern))\n",
-    "\n",
-    "print(\"Sample pattern: \" + str(pattern[int(sample_no)]))\n",
-    "print(\"Class of the above pattern: \" + str(classes[int(sample_no)]))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import numpy as np
+import math
+import random
 
+pattern = []
+classes = []
+
+filename = 'iris99.csv'
+file = open(filename,'r')
+#fopen=('iris99.csv','r')
+
+for line in file.readlines():
+    row = line.strip().split(',')
+    pattern.append(row[0:4])
+    classes.append(row[4])
+print("Iris Data Loaded")
+file.close
+
+pattern = np.array(pattern)
+
+sample_no = np.random.randint(0,len(pattern))
+
+print("Sample pattern: " + str(pattern[int(sample_no)]))
+print("Class of the above pattern: " + str(classes[int(sample_no)]))
