4869095c2590c4fcee26da1d6281998d9cf71b66
sssudarshan <56013986+sssudarshan@users.noreply.github.com>
2019-11-06 19:26:02 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -23,5 +23,20 @@ sample_no = np.random.randint(0,len(pattern))
 print("Sample pattern: " + str(pattern[int(sample_no)]))
 print("Class of the above pattern: " + str(classes[int(sample_no)]))
 
+##for calculating no. of maps unit
 
+def mapunits(ip_len,size='small'):
+    
+    hmap_units = 5*ip_len**0.54321
+     
+    if size == 'big':
+        hmap_units = 4*(hmap_units)
+    else:
+        hmap_units = 0.25*(hmap_units)
+        
+    return hmap_units
+        
+        
+map_units = mapunits(len(pattern),size='big')
+print("Heuristically computed appropriate no. of map units: "+str(int(map_units)))
 
